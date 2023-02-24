af38ffb69ff66831e199b6124530ea8dbd745358
SKY0FF <zaharka2901@gmail.com>
2023-02-24 19:16:25 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,56 @@
+import java.util.Optional;
+
+import static java.lang.System.out;
+public class main {
+    public static void main(String[] args) {
+        int sqrt_x = 555; // само число из которого нужно извлечь корень
+        int amount_sqrt_x = (int)(Math.ceil(Math.log10(sqrt_x))); // вычисляем сколько цифр в этом числе
+        int x = 0; // переменная для количества разрядов
+        int y = 1; // переменная для числа первого разряда из которого нужно вычислить корень
+        if(amount_sqrt_x%2 != 0) // определяет сколько разрядов
+        {
+            x = (amount_sqrt_x/2)+1;
+        }
+        else {
+            x = amount_sqrt_x/2;
+        }
+        for(int i = 1; i < x; i++) // определяет число первого разряда
+        {
+            y *= 100;
+        }
+        int firstrank = sqrt_x/y; // число первого разряда
+        int z = firstrank - sqrt(firstrank); // число первого разряда - корень из числа первого разряда
+        // ТАКЖЕ ЭТО ПЕРВОЕ ЧИСЛО ОТВЕТА
+        int z2 = (z*100) + (sqrt_x%100); // через 2 дня зашел в код, что это такое?
+
+        System.out.println(z2);
+        out.println(z); // вывод первого числа ответа
+        out.println(sqrt(3) + " " + sqrt_x/y);
+    }
+    public static int sqrt(int n){ // вычисляет корень не превышающий число
+        if(n == 1) // если импортированное число 1, то возвращаем 1
+        {
+            return 1;
+        }
+        else
+        {
+            int left_border = 0; // левая граница
+            int right_border = n; // правая граница
+            int temporarily = (left_border + right_border) / 2; // среднее между границ
+            int t = n; // вспомогательная перменная
+            for (;;) { // бесконечный цикл вычисляющий корень не превосходящий корня разрядов
+                temporarily = (right_border + left_border) / 2;
+                if (temporarily == t || temporarily * temporarily == n) {
+                    break;
+                }
+                if (temporarily * temporarily < n) {
+                    left_border = temporarily;
+                } else {
+                    right_border = temporarily;
+                }
+                t = temporarily;
+            }
+            return temporarily; // возврат корня не превосходящего корня разрядов
+        }
+    }
+}
