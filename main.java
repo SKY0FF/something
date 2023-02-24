f9632dbeb37dc1e840a756055c65d9c7a335f272
SKY0FF <zaharka2901@gmail.com>
2023-02-24 19:43:08 +0300

    Update main.java

--- main.java
+++ main.java
@@ -4,7 +4,9 @@ import static java.lang.System.out;
 public class main {
     public static void main(String[] args) {
         int sqrt_x = 555; // само число из которого нужно извлечь корень
+        out.println("1) Число из которого нужно извлечь корень: " + sqrt_x);
         int amount_sqrt_x = (int)(Math.ceil(Math.log10(sqrt_x))); // вычисляем сколько цифр в этом числе
+        out.println("2) Сколько цифр в числе из которого нужно извлечь корень: " + amount_sqrt_x);
         int x = 0; // переменная для количества разрядов
         int y = 1; // переменная для числа первого разряда из которого нужно вычислить корень
         if(amount_sqrt_x%2 != 0) // определяет сколько разрядов
@@ -14,11 +16,15 @@ public class main {
         else {
             x = amount_sqrt_x/2;
         }
+        out.println("3) Разрядов в этом числе: " + x);
         for(int i = 1; i < x; i++) // определяет число первого разряда
         {
             y *= 100;
         }
+        out.println("4) Это на сколько нужно делить число, чтобы получить число первого разряда: " + y);
         int firstrank = sqrt_x/y; // число первого разряда
+        out.println("5) Число первого разряда: " + firstrank);
+        out.println("6) Корень из числа первого разряда: " + sqrt(firstrank));
         int z = firstrank - sqrt(firstrank); // число первого разряда - корень из числа первого разряда
         // ТАКЖЕ ЭТО ПЕРВОЕ ЧИСЛО ОТВЕТА
         int z2 = (z*100) + (sqrt_x%100); // через 2 дня зашел в код, что это такое?
