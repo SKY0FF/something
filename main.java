ab525417e5df97e206f761d8194433f81847318f
SKY0FF <zaharka2901@gmail.com>
2023-02-25 14:57:33 +0300

    Update main.java

--- main.java
+++ main.java
@@ -3,29 +3,25 @@ import java.util.Optional;
 import static java.lang.System.out;
 public class main {
     public static void main(String[] args) {
-        int sqrt_x = 555; // само число из которого нужно извлечь корень
-        int amount_sqrt_x = (int)(Math.ceil(Math.log10(sqrt_x))); // вычисляем сколько цифр в этом числе
-        int x = 0; // переменная для количества разрядов
-        int y = 1; // переменная для числа первого разряда из которого нужно вычислить корень
-        if(amount_sqrt_x%2 != 0) // определяет сколько разрядов
+        int sqrt_x = 2655; // число из которого нужно извлечь корень
+        int amount_sqrt_x = (int)(Math.ceil(Math.log10(sqrt_x))); // количество цифр в числе из которого нужно извлечь корень
+        int x = 0; // вспомогательная переменная для количества разрядов
+        int y = 1; // переменная для определения на сколько нужно делить число, чтобы получить число первого разряда
+        if(amount_sqrt_x%2 != 0) // определение количества разрядов
         {
             x = (amount_sqrt_x/2)+1;
         }
         else {
             x = amount_sqrt_x/2;
         }
-        for(int i = 1; i < x; i++) // определяет число первого разряда
+        for(int i = 1; i < x; i++) // определение число первого разряда
         {
             y *= 100;
         }
         int firstrank = sqrt_x/y; // число первого разряда
-        int z = firstrank - sqrt(firstrank); // число первого разряда - корень из числа первого разряда
-        // ТАКЖЕ ЭТО ПЕРВОЕ ЧИСЛО ОТВЕТА
-        int z2 = (z*100) + (sqrt_x%100); // через 2 дня зашел в код, что это такое?
-
-        System.out.println(z2);
-        out.println(z); // вывод первого числа ответа
-        out.println(sqrt(3) + " " + sqrt_x/y);
+        // а число sqrt(firstrank) - первое число ответа
+        int z = firstrank - (sqrt(firstrank)*sqrt(firstrank)); // составляющее цифра к которой добавятся 2 цифры из другого разряда
+        out.println("Первое число ответа: " + sqrt(firstrank))
     }
     public static int sqrt(int n){ // вычисляет корень не превышающий число
         if(n == 1) // если импортированное число 1, то возвращаем 1
